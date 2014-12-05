56902737aa9f8ae5c1e2dc9932ff20391cd37cf9
chous <chous@acm-sl.org>
2014-12-05 19:17:29 +0000

    201412051917

--- src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
+++ src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
@@ -375,7 +375,7 @@ public class DockerfileMojo
         @NotNull final MavenProject target,
         @NotNull final String ownVersion)
     {
-        @NotNull final Map<String, String> input = new HashMap<String, String>();
+        @NotNull final Map<String, ?> input = new HashMap<String, Object>();
 
         input.put(Literals.T_U, target);
         input.put(Literals.VERSION_L, ownVersion);

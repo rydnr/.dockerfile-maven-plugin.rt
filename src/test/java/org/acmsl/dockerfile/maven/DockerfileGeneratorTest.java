3153ffe355a2225b8c6b839837bc7fa9dede2317
chous <chous@acm-sl.org>
2014-12-05 19:01:12 +0000

    201412051901

--- src/test/java/org/acmsl/dockerfile/maven/DockerfileGeneratorTest.java
+++ src/test/java/org/acmsl/dockerfile/maven/DockerfileGeneratorTest.java
@@ -91,7 +91,7 @@ public class DockerfileGeneratorTest
 
         input.put("key", testValue);
 
-        @NotNull final File template = File.createTempFile("Dockerfile", ".stg", new File(tempFolder.getFile()));
+        @NotNull final File template = Folder.newFile("Dockerfile.stg");
 
         @NotNull final FileUtils fileUtils = FileUtils.getInstance();
 

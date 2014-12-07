484fd9bba6ad59ad45ced056c6f5f754c48b75a1
chous <chous@acm-sl.org>
2014-12-07 07:22:26 +0000

    201412070722

--- src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
+++ src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
@@ -49,8 +49,9 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.deploy.AbstractDeployMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 

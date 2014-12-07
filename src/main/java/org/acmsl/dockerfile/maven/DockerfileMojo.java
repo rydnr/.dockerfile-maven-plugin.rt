24b0d123b90b41c6915db97a7898b8f142fae54b
chous <chous@acm-sl.org>
2014-12-07 08:56:28 +0100

    201412070856

--- src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
+++ src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
@@ -73,6 +73,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.text.regex.Matcher;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;

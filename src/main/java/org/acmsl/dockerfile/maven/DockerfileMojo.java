a6c51e3914d8d4e8e88b20689646d121fa4c4e12
chous <chous@acm-sl.org>
2015-01-17 10:34:00 +0100

    201501171034

--- src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
+++ src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
@@ -56,11 +56,11 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
 
 /*
  * Importing NotNull annotations.
  */
-import org.apache.maven.repository.RepositorySystem;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 

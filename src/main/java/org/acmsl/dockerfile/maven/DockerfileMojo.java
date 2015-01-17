5ce0b57e1609945446e0f39078a6d5ca23cadda8
chous <chous@acm-sl.org>
2015-01-17 12:01:25 +0100

    201501171201

--- src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
+++ src/main/java/org/acmsl/dockerfile/maven/DockerfileMojo.java
@@ -559,7 +559,7 @@ public class DockerfileMojo
 
         if (property == null)
         {
-            result = immutableGetDeploymenRetries();
+            result = immutableGetDeploymentRetries();
         }
         else
         {
